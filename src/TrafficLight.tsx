33bd2f9d52cab4c6d6620b5b0e492022474c6b95
couvq <quentincouvelaire@gmail.com>
2026-06-30 16:13:16 -0400

    refactor to remove duplicated code for lights

--- src/TrafficLight.tsx
+++ src/TrafficLight.tsx
@@ -9,42 +9,15 @@ const Light = ({ color }: LightProps) => {
   return <div className={`light light_${color}`}></div>;
 };
 
-const GreenLight = () => {
-  const snapshot = useTrafficLightSnapshot();
-  const isLightGreen = snapshot.matches("green");
-  if (isLightGreen) {
-    return <Light color="green" />;
-  } else {
-    return <Light color="grey" />;
-  }
-};
-
-const YellowLight = () => {
-  const snapshot = useTrafficLightSnapshot();
-  const isLightYellow = snapshot.matches("yellow");
-  if (isLightYellow) {
-    return <Light color="yellow" />;
-  } else {
-    return <Light color="grey" />;
-  }
-};
-
-const RedLight = () => {
-  const snapshot = useTrafficLightSnapshot();
-  const isLightRed = snapshot.matches("red");
-  if (isLightRed) {
-    return <Light color="red" />;
-  } else {
-    return <Light color="grey" />;
-  }
-};
-
 const TrafficLight = () => {
+  const snapshot = useTrafficLightSnapshot();
+  const activeColor = snapshot.value as TrafficLightColor
+
   return (
     <div className="traffic_light">
-      <GreenLight />
-      <YellowLight />
-      <RedLight />
+      {["green", "yellow", "red"].map((c) => (
+        <Light key={c} color={c === activeColor ? c : 'grey'}/>
+      ))}
     </div>
   );
 };
